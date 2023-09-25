3e8fa5e4f1cd52b81056bddbed31fdfc7d76c67d
Oscar Alonso Rueda Ruiz <orueda@sancsoft.com>
2023-09-25 15:29:26 -0500

    updates with Topshelf

---
+++ HangfireService/HangfireService.cs
@@ -0,0 +1,32 @@
+﻿using Hangfire;
+
+namespace HangfireConsole
+{
+    public class HangfireService
+    {
+        private BackgroundJobServer _server;
+
+        public HangfireService()
+        {
+            GlobalConfiguration.Configuration
+                .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+                .UseColouredConsoleLogProvider()
+                .UseSimpleAssemblyNameTypeSerializer()
+                .UseRecommendedSerializerSettings()
+                .UseSqlServerStorage("Server=localhost;Initial Catalog=pruebas;User ID=sa;Password=Pass@Word;Encrypt=True;TrustServerCertificate=True;");
+
+            _server = new BackgroundJobServer();
+            BackgroundJob.Enqueue(() => Console.WriteLine("Hello, world from console and service!"));
+        }
+
+        public void Start()
+        {
+            _server.Start();
+        }
+
+        public void Stop()
+        {
+            _server.Dispose();
+        }
+    }
+}
--- HangfireService/Program.cs
+++ HangfireService/Program.cs
@@ -1,16 +1,18 @@
-﻿using Hangfire;
+﻿using Topshelf;
 
 
-GlobalConfiguration.Configuration
-    .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
-    .UseColouredConsoleLogProvider()
-    .UseSimpleAssemblyNameTypeSerializer()
-    .UseRecommendedSerializerSettings()
-    .UseSqlServerStorage("Server=localhost;Initial Catalog=pruebas;User ID=sa;Password=Pass@Word;Encrypt=True;TrustServerCertificate=True;");
-
-BackgroundJob.Enqueue(() => Console.WriteLine("Hello, world!"));
-
-using (var server = new BackgroundJobServer())
+var rc = HostFactory.Run(x =>
 {
-    Console.ReadLine();
-}
+    x.Service<HangfireConsole.HangfireService>(s =>
+    {
+        s.ConstructUsing(name => new HangfireConsole.HangfireService());
+        s.WhenStarted(tc => tc.Start());
+        s.WhenStopped(tc => tc.Stop());
+    });
+    x.RunAsLocalSystem();
+
+    x.SetDescription("Hangfire Topshelf Service");
+    x.SetDisplayName("HangfireService");
+    x.SetServiceName("HangfireService");
+});
+
