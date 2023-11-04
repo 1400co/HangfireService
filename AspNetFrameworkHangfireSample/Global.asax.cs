d4317840fb4965e9c3dcedb8dc55b597db1f780a
Oscar Alonso Rueda Ruiz <orueda@sancsoft.com>
2023-11-04 11:02:56 -0500

    added startup to asp.net freamework

--- AspNetFrameworkHangfireSample/Global.asax.cs
+++ AspNetFrameworkHangfireSample/Global.asax.cs
@@ -1,7 +1,6 @@
 ﻿using Hangfire;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -11,22 +10,23 @@ namespace AspNetFrameworkHangfireSample
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
+        //Approach 1
         //Use this if worker will run on website.
-        private IEnumerable<IDisposable> GetHangfireServers()
-        {
-            GlobalConfiguration.Configuration
-                .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
-                .UseSimpleAssemblyNameTypeSerializer()
-                .UseRecommendedSerializerSettings()
-                .UseSqlServerStorage("Server=localhost;Initial Catalog=pruebas;User ID=sa;Password=Pass@Word;Encrypt=True;TrustServerCertificate=True;");
+        //private IEnumerable<IDisposable> GetHangfireServers()
+        //{
+        //    GlobalConfiguration.Configuration
+        //        .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+        //        .UseSimpleAssemblyNameTypeSerializer()
+        //        .UseRecommendedSerializerSettings()
+        //        .UseSqlServerStorage("Server=localhost;Initial Catalog=pruebas;User ID=sa;Password=Pass@Word;Encrypt=True;TrustServerCertificate=True;");
 
-            var serverOptions = new BackgroundJobServerOptions
-            {
-                WorkerCount = 1
-            };
+        //    var serverOptions = new BackgroundJobServerOptions
+        //    {
+        //        WorkerCount = 1
+        //    };
 
-            yield return new BackgroundJobServer(serverOptions);
-        }
+        //    yield return new BackgroundJobServer(serverOptions);
+        //}
 
         protected void Application_Start(object sender, EventArgs e)
         {
@@ -36,10 +36,11 @@ namespace AspNetFrameworkHangfireSample
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
-            HangfireStartup.ConfigureHangfire(); // Use this if the hangfire server is located outside of web api.
-            HangfireAspNet.Use(GetHangfireServers); //Use this to create the server on website.
+            //Approach 1
+            //HangfireStartup.ConfigureHangfire(); // Use this if the hangfire server is located outside of web api.
+            //HangfireAspNet.Use(GetHangfireServers); //Use this to create the server on website.
             //// Let's also create a sample background job
-            BackgroundJob.Enqueue(() => Debug.WriteLine("Hello world from Hangfire .net Framework!"));
+            //BackgroundJob.Enqueue(() => Debug.WriteLine("Hello world from Hangfire .net Framework!"));
         }
     }
 }
---
+++ AspNetFrameworkHangfireSample/Startup.cs
@@ -0,0 +1,36 @@
+﻿using Hangfire;
+using Microsoft.Owin;
+using Owin;
+using System.Collections.Generic;
+using System;
+using System.Diagnostics;
+
+namespace AspNetFrameworkHangfireSample
+{
+    public class Startup
+    {
+        private IEnumerable<IDisposable> GetHangfireServers()
+        {
+            GlobalConfiguration.Configuration
+                .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+                .UseSimpleAssemblyNameTypeSerializer()
+                .UseRecommendedSerializerSettings()
+                .UseSqlServerStorage("Server=localhost;Initial Catalog=pruebas;User ID=sa;Password=Pass@Word;Encrypt=True;TrustServerCertificate=True;");
+
+            var serverOptions = new BackgroundJobServerOptions
+            {
+                WorkerCount = 1
+            };
+
+            yield return new BackgroundJobServer(serverOptions);
+        }
+
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseHangfireAspNet(GetHangfireServers);
+            app.UseHangfireDashboard();
+
+            BackgroundJob.Enqueue(() => Debug.WriteLine("Hello world from Hangfire!"));
+        }
+    }
+}
